0fc38feb23a4730dc3ce715f9b306f08d4c05b76
Tisdale Fry <tisdalefry@gmail.com>
2020-02-24 22:50:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,7 @@ def monopoly_with_third_tier
       :monday => "2",
       :tuesday => "1",
       :wednesday => "3",
-      :one_piece_owned => "25"  
+      :one_piece_owned => 25  
     }
   }
 	  
